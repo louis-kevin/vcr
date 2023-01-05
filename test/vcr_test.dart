ddd21f64310491cdb3ac503e16e574612b5ba080
Kevin Louis <kevinlouis.dev@gmail.com>
2023-01-05 00:59:38 -0300

    update tests

--- test/vcr_test.dart
+++ test/vcr_test.dart
@@ -9,17 +9,10 @@ import 'package:vcr/vcr.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  VcrAdapter adapter;
-  Dio client;
-
-  File getFile() {
-    String path = 'github/user_repos.json';
-    Directory current = Directory.current;
-    String finalPath = current.path.endsWith('/test') ? current.path : current.path + '/test';
-
-    finalPath = "$finalPath/cassettes/$path";
-    return File(finalPath);
-  }
+  late VcrAdapter adapter;
+  late Dio client;
+  String cassetteName = 'github/user_repos';
+  String url = 'https://api.github.com/users/louis-kevin/repos';
 
   List _readFile(File file) {
     String jsonString = file.readAsStringSync();
@@ -33,26 +26,23 @@ void main() {
   }
 
   setUp(() {
-    adapter = VcrAdapter();
+    adapter = VcrAdapter(basePath: 'test/cassettes', createIfNotExists: false);
     client = Dio();
     client.httpClientAdapter = adapter;
   });
 
   tearDown(() {
-    Directory current = Directory.current;
-    String finalPath = current.path.endsWith('/test') ? current.path : current.path + '/test';
-    var directory = Directory('$finalPath/cassettes');
-    if (directory.existsSync()) directory.delete(recursive: true);
+    File file = adapter.loadFile(cassetteName);
+    if (file.existsSync()) file.parent.delete(recursive: true);
   });
 
   test('make request when there is no cassette', () async {
-    File file = getFile();
+    File file = adapter.loadFile(cassetteName);
     expect(file.existsSync(), isFalse);
 
-    await adapter.useCassette('github/user_repos');
+    await adapter.useCassette(cassetteName);
 
-    Response response =
-        await client.get('https://api.github.com/users/keviinlouis/repos');
+    Response response = await client.get(url);
     expect(response.statusCode, 200);
 
     expect(file.existsSync(), isTrue);
@@ -62,35 +52,66 @@ void main() {
 
   test('must not store a new request in same file when it already exists',
       () async {
-    File file = getFile();
+    File file = adapter.loadFile(cassetteName);
     expect(file.existsSync(), isFalse);
-    await adapter.useCassette('github/user_repos');
+    await adapter.useCassette(cassetteName);
 
-    Response response =
-        await client.get('https://api.github.com/users/keviinlouis/repos');
+    Response response = await client.get(url);
     expect(response.statusCode, 200);
     expect(file.existsSync(), isTrue);
     checkRequestSizeInFile(file, 1);
 
-    response =
-        await client.get('https://api.github.com/users/keviinlouis/repos');
+    response = await client.get(url);
     expect(response.statusCode, 200);
     checkRequestSizeInFile(file, 1);
   });
 
   test('must store a new request in same file when does not found', () async {
-    File file = getFile();
+    File file = adapter.loadFile(cassetteName);
     expect(file.existsSync(), isFalse);
-    await adapter.useCassette('github/user_repos');
+    await adapter.useCassette(cassetteName);
 
-    Response response =
-        await client.get('https://api.github.com/users/keviinlouis/repos');
+    Response response = await client.get(url);
     expect(response.statusCode, 200);
     expect(file.existsSync(), isTrue);
     checkRequestSizeInFile(file, 1);
 
-    response = await client.get('https://api.github.com/users/keviinlouis');
+    response = await client.get('https://api.github.com/users/louis-kevin');
     expect(response.statusCode, 200);
     checkRequestSizeInFile(file, 2);
+  });
+
+  test('must create cassette when useCassette is not called', () async {
+    var oldCreateIfNotExists = adapter.createIfNotExists;
+    adapter.createIfNotExists = true;
+    File file = adapter.loadFile('users/louis-kevin/repos.json');
+
+    expect(file.existsSync(), isFalse);
+
+    Response response = await client.get(url);
+    expect(response.statusCode, 200);
+    expect(file.existsSync(), isTrue);
+    checkRequestSizeInFile(file, 1);
+
+    response = await client.get(url);
+    expect(response.statusCode, 200);
+    checkRequestSizeInFile(file, 1);
+
+    file.deleteSync();
+    expect(file.existsSync(), isFalse);
+    adapter.createIfNotExists = oldCreateIfNotExists;
+  });
+
+  test('must not create cassette when useCassette is not called', () async {
+    var oldCreateIfNotExists = adapter.createIfNotExists;
+    adapter.createIfNotExists = false;
+    File file = adapter.loadFile('users/louis/kevin/repos.json');
+
+    expect(file.existsSync(), isFalse);
+
+    expect(() => client.get(url), throwsException);
+
+    expect(file.existsSync(), isFalse);
+    adapter.createIfNotExists = oldCreateIfNotExists;
   });
 }
